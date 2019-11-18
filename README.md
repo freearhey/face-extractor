6ec93a9847311b2b6e761b0040927f7e37262ce7
freearhey <free.arhey@gmail.com>
2019-11-18 05:27:52 +0300

    Init

---
+++ README.md
@@ -0,0 +1,59 @@
+# face-extractor
+
+![Illustration of how face-extractor works](illustration.png)
+
+Python script that detect faces on the image or video, extracts them and saves to the specified folder.
+
+## Installation
+
+Copy repository to your computer using one of the available methods. For example, this can be done using the `git clone` command:
+
+```sh
+git clone https://github.com/freearhey/face-extractor.git
+```
+
+Then you need to go to the project folder and install all the dependencies:
+
+```sh
+# change directory
+cd face-extractor
+
+# install dependencies
+pip install -r requirements.txt
+```
+
+And you're done.
+
+## Usage
+
+To run the script you need to pass only the path to the image that need to be processed, as well as the path to the folder where the extracted faces will be saved.
+
+```sh
+python extract.py --input path/to/image.jpg --output path/to/output_folder
+```
+
+ The video file can also be used as the input:
+
+```sh
+python extract.py --input path/to/video.mp4 --output path/to/output_folder
+```
+
+Or it could be a folder containing these files:
+
+```sh
+python extract.py --input path/to/folder_with_images
+```
+
+By default, the files are saved in the `output` folder.
+
+## Demo
+
+In the `examples` folder you can find several images that can be processed using the script, like so:
+
+```sh
+python extract.py --input examples
+```
+
+## License
+
+[MIT](LICENSE)
---
+++ extract.py
@@ -0,0 +1,96 @@
+# USAGE
+# python extract.py --output outputDir/ --input someDir/
+# python extract.py --output outputDir/ --input image.png
+# python extract.py --output outputDir/ --input video.mp4
+
+import os
+import cv2
+import argparse
+import filetype
+import cvlib as cv
+
+def main(args):
+  input = args["input"]
+  isDirectory = os.path.isdir(input)
+  sources = []
+  if isDirectory:
+    files = []
+    for path in os.listdir(input):
+      file = os.path.join(input, path)
+      if os.path.isfile(file):
+        files.append(file)
+    sources.extend(files)
+  else:
+    sources.append(input)
+
+  images = []
+  for path in sources:
+    kind = filetype.guess(path)
+    if kind is None:
+      continue
+    if kind.mime.startswith('video'):
+      print('[INFO] extracting frames from video...')
+      video = cv2.VideoCapture(path)
+      while True:
+        success, frame = video.read()
+        if success:
+          image = {
+            "file": frame,
+            "source": path,
+            "sourceType": "video",
+            "filename": os.path.splitext(os.path.basename(path))[0]
+          }
+          images.append(image)
+        else:
+          break
+      video.release()
+      cv2.destroyAllWindows()
+    elif kind.mime.startswith('image'):
+      image = {
+        "file": cv2.imread(path),
+        "source": path,
+        "sourceType": "image",
+        "filename": os.path.splitext(os.path.basename(path))[0]
+      }
+      images.append(image)
+
+  cwd = os.getcwd()
+  outputDir = os.path.join(cwd, args["output"])
+  if not os.path.exists(outputDir):
+    os.makedirs(outputDir)
+
+  total = 0
+  for (i, image) in enumerate(images):
+    print("[INFO] processing image {}/{}".format(i + 1, len(images)))
+
+    results, confidences = cv.detect_face(image["file"]) 
+    
+    for (j, bounds) in enumerate(results):
+      (startX, startY, endX, endY) = bounds
+      face = image["file"][startY:endY, startX:endX]
+      (fH, fW) = face.shape[:2]
+      
+      if fW < 10 or fH < 10:
+        continue
+
+      outputFilename = ''
+      if image["sourceType"] == "video":
+        outputFilename = '{}_{:04d}_{}.jpg'.format(image["filename"], i, j)
+      else:
+        outputFilename = '{}_{}.jpg'.format(image["filename"], j)
+      outputPath = os.path.join(outputDir, outputFilename)
+      cv2.imwrite(outputPath, face)
+      total += 1
+
+  print("[INFO] found {} face(s)".format(total))
+
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+  
+  # options
+  parser.add_argument("-i", "--input", required=True, help="path to input directory or file")
+  parser.add_argument("-o", "--output", default="output", help="path to output directory of faces")
+  
+  args = vars(parser.parse_args())
+  main(args)
